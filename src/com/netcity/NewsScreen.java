b8e83565f61eba9eaf8277736693424f3902de8c
Sergey <kozelkoS@yandex.ru>
2014-03-12 18:37:38 +1100

    Small fixes

--- src/com/netcity/NewsScreen.java
+++ src/com/netcity/NewsScreen.java
@@ -27,7 +27,7 @@ public class NewsScreen extends Fragment {
 	Button btnSchedule, btnMarks;
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.activity_news_screen, null);
+		View v = inflater.inflate(R.layout.news_screen, null);
 		return v;
 	}
 
